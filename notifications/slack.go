c7d9e52d6981d74baf8689b31bcf2af4003ea338
BoikenMac <do@hercle.financial>
2025-09-02 16:20:24 +0200

    WaitingForPluginOperations

--- notifications/slack.go
+++ notifications/slack.go
@@ -77,6 +77,10 @@ func (s *SlackNotifier) Notify(message string) error {
 			displayName: "Finalizing Partially Failed",
 			color:       "#FFFF00", // Yellow for finalizing partially failed state
 		},
+		"waitingforpluginoperations": {
+			displayName: "Waiting For Plugin Operations",
+			color:       "#FFFF00", // Yellow for Waiting For Plugin Operations state
+		},		
 		"finalizing": {
 			displayName: "Finalizing",
 			color:       "#025A13", // Dark green for finalizing state
@@ -99,6 +103,8 @@ func (s *SlackNotifier) Notify(message string) error {
 		backupStatus = "partiallyfailed"
 	case strings.Contains(lowerMsg, "finished with status: finalizingpartiallyfailed"):
 		backupStatus = "finalizingpartiallyfailed"
+	case strings.Contains(lowerMsg, "finished with status: waitingforpluginoperations"):
+		backupStatus = "waitingforpluginoperations"		
 	case strings.Contains(lowerMsg, "finished with status: finalizing"):
 		backupStatus = "finalizing"
 	default:
